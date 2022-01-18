d40723c4127cd58da9efd75070ab2e2ceef18437
tylerstohlman <tyler.stohlman@kellogg.northwestern.edu>
2022-01-18 17:26:53 +0000

    In Class Exercises

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,59 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS industry_memberships;
+
 -- CREATE TABLES
+
+CREATE TABLE salespersons (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT,
+last_name TEXT,
+title TEXT,
+email TEXT
+);
+
+CREATE TABLE contacts (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT,
+last_name TEXT,
+title TEXT,
+email TEXT,
+phone_number INTEGER, 
+company_id TEXT,
+salesperson_id TEXT
+);
+
+CREATE TABLE industries(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT
+);
+
+CREATE TABLE companies (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+industry_id INTEGER
+);
+
+CREATE TABLE activities (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+date INTEGER,
+category TEXT,
+time INTEGER,
+content TEXT,
+company_id INTEGER,
+industry_id INTEGER,
+salesperson_id INTEGER,
+contact_id INTEGER
+);
+
+CREATE TABLE industry_memberships (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+company_id INTEGER,
+industry_id INTEGER
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,39 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS followers;
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT,
+  first_name TEXT,
+  last_name TEXT,
+  location TEXT
+);
+
+CREATE TABLE posts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  image TEXT,
+  created_at TEXT
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  post_id INTEGER
+);
+
+CREATE TABLE comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  post_id INTEGER,
+  content TEXT
+);
+
+CREATE TABLE followers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  followee_user_id INTEGER,
+  follower_user_id INTEGER
+);
